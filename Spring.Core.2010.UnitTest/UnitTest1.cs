e9c730e85d1ea32421bd59cc25fa4668e2bd022b
liuyb <liuyb@Ctrip.com>
2015-08-04 17:31:18 +0800

    add UnitTest

---
+++ Spring.Core.2010.UnitTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Spring.Context;
+using Spring.Context.Support;
+using Spring.Core.UnitTest;
+
+namespace Spring.Core._2010.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void DefaultApplicationContext()
+        {
+       
+            IApplicationContext ctx = ContextRegistry.GetContext();
+            var entity = ctx.GetObject("personDao") as PersonDao;
+            entity.Save();
+        }
+    }
+}
